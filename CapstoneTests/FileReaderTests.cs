d7ae2e1d13e002858993328ca36b3e524c2a361d
Brad Azich <azichbrad@gmail.com>
2020-06-07 17:48:24 -0400

    All tests working

---
+++ CapstoneTests/FileReaderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    [TestClass]
+    public class FileReaderTests
+    {
+        [TestMethod]
+        public void UsesCorrectPath()
+        {
+            FileReader correctPath = new FileReader();
+            Assert.AreEqual(Environment.CurrentDirectory, FileReader.directoryPath);
+        }
+        [TestMethod]
+        public void GetsCorrectFile()
+        {
+            FileReader correctFile = new FileReader();
+            Assert.AreEqual("vendingmachine.csv", FileReader.fileName);
+        }
+
+    }
+}
